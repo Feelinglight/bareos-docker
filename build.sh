babb0ce23dc9feb569a4f8c8fcb1a211447951f3
feelinglight <kolpacc123@mail.ru>
2025-08-23 14:24:04 +0500

    Рефакторинг скрипта сборки

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Этой версией будут тэгнуты докер образы
 BAREOS_VERSION="24.0.5~pre32.7c5f79a1e"
 # Дистрибутив, для которого собраны пакеты bareos
@@ -8,21 +10,22 @@ DISTRO="xUbuntu_24.04"
 # NOCACHE="--no-cache"
 NOCACHE=
 
-docker build "${NOCACHE}" -t feelinglight/bareos-dir:${BAREOS_VERSION} -t feelinglight/bareos-dir:latest --target bareos-dir .
-docker build "${NOCACHE}" -t feelinglight/bareos-webui:${BAREOS_VERSION} -t feelinglight/bareos-webui:latest --target bareos-webui .
-docker build "${NOCACHE}" -t feelinglight/bareos-fd:${BAREOS_VERSION} -t feelinglight/bareos-fd:latest --target bareos-fd .
-docker build "${NOCACHE}" -t feelinglight/bareos-sd:${BAREOS_VERSION} -t feelinglight/bareos-sd:latest --target bareos-sd .
+bareos_daemons="bareos-dir bareos-webui bareos-fd bareos-sd"
 
-docker rmi "$(docker images --filter "dangling=true" -q --no-trunc)"
+for daemon in $bareos_daemons; do
+  # shellcheck disable=SC2086
+  docker build ${NOCACHE} \
+    --build-arg "distro=$DISTRO" \
+    --target "${daemon}" \
+    -t "feelinglight/${daemon}:${BAREOS_VERSION}" \
+    -t "feelinglight/${daemon}:latest" \
+    .
+done
 
-docker push feelinglight/bareos-dir:${BAREOS_VERSION}
-docker push feelinglight/bareos-dir:latest
+# shellcheck disable=SC2046
+docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
 
-docker push feelinglight/bareos-sd:${BAREOS_VERSION}
-docker push feelinglight/bareos-sd:latest
-
-docker push feelinglight/bareos-fd:${BAREOS_VERSION}
-docker push feelinglight/bareos-fd:latest
-
-docker push feelinglight/bareos-webui:${BAREOS_VERSION}
-docker push feelinglight/bareos-webui:latest
+for daemon in $bareos_daemons; do
+  docker push "feelinglight/${daemon}:${BAREOS_VERSION}"
+  docker push "feelinglight/${daemon}:latest"
+done
